b4b4047d49feef333e74eb1cd1d0142229e0bd50
Mohamed Aziz Belkhouja <belkhoujamohamedaziz@gmail.com>
2024-08-09 12:32:25 +0100

    class CarRentalSystem

--- Main.java
+++ Main.java
@@ -1,5 +1,6 @@
 import java.util.arraylist;
 import java.util.Scanner;
+import java.util.ArrayList;
 import java.util.List;
 
 class Car {
@@ -89,6 +90,52 @@ class Rental {
     }
 }
 
+class CarRentalSystem {
+    private List<Car> cars;
+    private List<Customer> customers;
+    private List<Rental> rentals;
+
+    public CarRentalSystem() {
+        cars = new ArrayList<>();
+        customers = new ArrayList<>();
+        rentals = new ArrayList<>();
+    }
+
+    public void addCar(Car car) {
+        cars.add(car);
+    }
+
+    public void addCustomer(Customer customer) {
+        customers.add(customer);
+    }
+
+    public void rentCar(Car car, Customer customer, int days) {
+        if (car.isAvailable()) {
+            car.rent();
+            rentals.add(new Rental(car, customer, days));
+
+        } else {
+            System.out.println("Car is not available for rent.");
+        }
+    }
+
+    public void returnCar(Car car) {
+        car.returnCar();
+        Rental rentalToRemove = null;
+        for (Rental rental : rentals) {
+            if (rental.getCar() == car) {
+                rentalToRemove = rental;
+                break;
+            }
+        }
+        if (rentalToRemove != null) {
+            rentals.remove(rentalToRemove);
+
+        } else {
+            System.out.println("Car was not rented.");
+        }
+    }
+}
 
 
 
