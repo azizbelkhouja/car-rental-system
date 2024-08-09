cedb69addc938d4bdd2bc91708fab2565da3a5a5
Mohamed Aziz Belkhouja <belkhoujamohamedaziz@gmail.com>
2024-08-09 12:19:41 +0100

    class Car

--- Main.java
+++ Main.java
@@ -8,27 +8,46 @@ class Car {
     private String model;
     private double basePricePerDay;
     private boolean isAvailable;
+
+    public Car(String carId, String brand, String model, double basePricePerDay) {
+        this.carId = carId;
+        this.brand = brand;
+        this.model = model;
+        this.basePricePerDay = basePricePerDay;
+        this.isAvailable = true;
+    }
+
+    public String getCarId() {
+        return carId;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public double caclculatePrice(int rentalDays) {
+        return basePricePerDay * rentalDays;
+    }
+
+    public boolean isAvailable() {
+        return isAvailable;
+    }
+
+    public void rent() {
+        isAvailable = false;
+    }
+
+    public void returnCar() {
+        isAvailable = true;
+    }
+
 }
 
-public Car(String carId, String brand, String model, double basePricePerDay) {
-    this.carId = carId;
-    this.brand = brand;
-    this.model = model;
-    this.basePricePerDay = basePricePerDay;
-    this.isAvailable = true;
-}
 
-public String getCarId() {
-    return carId;
-}
-
-public String getBrand() {
-    return brand;
-}
-
-public String getModel() {
-    return model;
-}
 
 
 
