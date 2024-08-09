249203851a72a1a3c1f40a63482648b48bc89b42
Mohamed Aziz Belkhouja <belkhoujamohamedaziz@gmail.com>
2024-08-09 12:28:10 +0100

    class Rental

--- Main.java
+++ Main.java
@@ -65,6 +65,30 @@ class Customer {
     }
 }
 
+class Rental {
+    private Car car;
+    private Customer customer;
+    private int days;
+
+    public Rental(Car car, Customer customer, int days) {
+        this.car = car;
+        this.customer = customer;
+        this.days = days;
+    }
+
+    public Car getCar() {
+        return car;
+    }
+
+    public Customer getCustomer() {
+        return customer;
+    }
+
+    public int getDays() {
+        return days;
+    }
+}
+
 
 
 
